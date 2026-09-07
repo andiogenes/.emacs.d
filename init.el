dbb71fffbdf0a14af021ce91e7950486ac2d89e6
Anton Zavyalov <megadeathlightsaber@gmail.com>
2026-09-08 01:34:59 +0700

    Tweak font size

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
 
 ;;; Font
 
-(let ((font-default "JetBrains Mono 12"))
+(let ((font-default "JetBrains Mono 14"))
   (set-face-attribute 'default nil :font font-default)
   (set-face-attribute 'fixed-pitch nil :font font-default))
 
